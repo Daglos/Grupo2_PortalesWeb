2df01d89bf6eb149bfe9215206b8835e6868fdf2
Emelyram <emem_ramirezv@unicah.edu>
2025-04-07 09:43:40 -0600

    evento resaltar

--- JAVASCRIPT/blog.js
+++ JAVASCRIPT/blog.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Menú hamburguesa
     const menuBtn = document.getElementById("menu-hamburguesa");
     const navMenu = document.getElementById("nav-menu").querySelector("ul");
 
     menuBtn.addEventListener("click", () => {
         navMenu.classList.toggle("active");
     });
+
+    // Resaltado visual 
+    const cajasInfo = document.querySelectorAll(".info-box");
+
+    cajasInfo.forEach(caja => {
+        caja.addEventListener("click", () => {
+            caja.classList.toggle("resaltado");
+        });
+    });
 });
+
 
