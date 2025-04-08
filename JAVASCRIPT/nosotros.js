064cd85ead1290346c506780fcdb48d50761a0b6
Emelyram <emem_ramirezv@unicah.edu>
2025-04-07 21:58:07 -0600

    cambio padding

--- JAVASCRIPT/nosotros.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnMenu = document.getElementById("menu-hamburguesa");
-    const nav = document.getElementById("nav-menu");
-
-    btnMenu.addEventListener("click", () => {
-        nav.classList.toggle("mostrar");
-    });
-});
